718e3e35d78e5777fe25ae8efa66ce37aeaff2c6
Khalil Garaali <112202385+KhalilGarali@users.noreply.github.com>
2023-08-27 16:10:01 -0400

    Time: 17 ms (61.31%), Space: 13.3 MB (53.96%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -5,21 +5,6 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        # pointer = len(nums)-1
-        # for i in range(len(nums)):
-        #     if pointer == i:
-        #         break
-        #     if nums[pointer] == val:
-        #         pointer-=1
-        #     if nums[i] != val:
-        #         print("Skipping iteration because it is not the same value")
-        #         continue
-        #     if nums[i] == val:
-        #         nums[i] = nums[pointer]
-        #         pointer-=1
-        #         print("Pointer index: " + str(pointer))
-        #         print("Iteration index: " + str(i))
-        # return pointer+1
         i = 0
         for x in nums:
             if x != val:
